5839b42ede2b8659223d31d3399f3ba293a2c5a6
vlenix2505 <belenantuanet.007@gmail.com>
2024-11-08 23:59:19 -0500

    feat(frontend): Add resources for user visualization

--- src/app/core/services/content.service.ts
+++ src/app/core/services/content.service.ts
@@ -1,19 +1,75 @@
 import {inject, Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Content} from '../model/content';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
+
   private url = environment.apiUrl
   private http: HttpClient = inject(HttpClient);
 
   constructor() { }
 
-  list():Observable<any>{
+  list(): Observable<any>{
     return this.http.get(this.url + "/contents");
   }
 
+  listByTitle(title: String): Observable<any>{
+    return this.http.get(this.url + "/contents/title/" + title,{});
+  }
+
+  listContentByLevels(level: String): Observable<any>{
+    return this.http.get(this.url + "/content_level/" + level, {});
+  }
+
+  listContentByTypes(type: String): Observable<any>{
+    return this.http.get(this.url + "/content_type/" + type, {});
+  }
+
+  listContentByTheme(theme: String): Observable<any>{
+    return this.http.get(this.url + "/content_theme/" + theme, {});
+  }
+
+  listContentByLevelsAndTypes(level: String, type: String): Observable<any>{
+    return this.http.get(this.url + "/content_level_type/" + level + "/"+ type, {});
+  }
+
+  listContentByLevelsAndTheme(level: String, theme: String): Observable<any>{
+    return this.http.get(this.url + "/content_theme_level/" + level + "/"+ theme, {});
+  }
+
+  getContentById(id: number): Observable<Content> {
+    return this.http.get<Content>(`${this.url}/content/id/${id}`);
+  }
+
+
+  listFilterContent(level?: string, theme?: string, type?: string): Observable<any>{
+
+    const paramsObj: any = {};
+
+    if (level) {
+      paramsObj['level'] = level;
+    }
+    if (type) {
+      paramsObj['type'] = type;
+    }
+    if (theme) {
+      paramsObj['theme'] = theme;
+    }
+
+    const params = new HttpParams({ fromObject: paramsObj });
+    return this.http.get(this.url + "/contents/filter", {params});
+  }
+
+  listContentOrderByDateOfPublicationDesc(): Observable<any>{
+    return this.http.get(this.url + "/content_fechaDesc");
+  }
+  listContentOrderByDateOfPublicationAsc(): Observable<any>{
+    return this.http.get(this.url + "/content_fechaAsc");
+
+  }
 }
