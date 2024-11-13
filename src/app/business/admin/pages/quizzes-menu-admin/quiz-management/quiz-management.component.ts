6ecf3244fda66dd154eee5a5112fd9b0a090e49e
CamiTorres22 <u202217059@upc.edu.pe>
2024-11-12 23:58:52 -0500

    feat (frontend): Add quizzes visualization

---
+++ src/app/business/admin/pages/quizzes-menu-admin/quiz-management/quiz-management.component.ts
@@ -0,0 +1,69 @@
+import {Component, inject, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {Quiz} from '../../../../../core/model/quiz';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {Router} from '@angular/router';
+import {QuizService} from '../../../../../core/services/quiz.service';
+
+@Component({
+  selector: 'app-quiz-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-management.component.html',
+  styleUrl: './quiz-management.component.css'
+})
+export class QuizManagementComponent {
+  lista: Quiz[]=[];
+  displayedColumns: string[]=['id', 'TotalPoints','iCreatedAt', 'user'];
+  dataSource: MatTableDataSource<Quiz>= new MatTableDataSource<Quiz>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  quizAdminService: QuizService = inject(QuizService);
+  router: Router = inject(Router);
+
+  constructor() {
+    console.log("Load constructor!")
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit(): void {
+    console.log("Load Lista!");
+    this.loadLista();
+  }
+
+  private loadLista():void {
+    this.quizAdminService.list().subscribe({
+      next: (data) => {
+        this.dataSource.data = data;
+        console.log('Data recibida:', data);  // Asegúrate de que el array de levels esté presente en los datos recibidos
+      },
+      error: (error) => console.log("Error e en consulta",error),
+    });
+    console.log(this.dataSource.data);
+  }
+  onSearch(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const userName = inputElement.value;
+
+    if(userName != ''){
+      console.log(userName);
+      this.quizAdminService.listByUser(userName).subscribe({
+        next: (data) => {
+          this.dataSource.data = data;
+          console.log('Data filtrada recibida');
+        },
+        error: (error) => console.log("Error en consulta"),
+      });
+    }
+    else{
+      this.loadLista();
+    }
+  }
+}
--- src/app/core/services/quiz.service.ts
+++ src/app/core/services/quiz.service.ts
@@ -11,17 +11,26 @@ export class QuizService {
   private http: HttpClient = inject(HttpClient);
 
   constructor() { }
+ //admin
   list():Observable<any>{
     return this.http.get(this.url + "/quizzes");
   }
   listByUser(userName: String): Observable<any>{
     return this.http.get(this.url + "/quizzes/username/" + userName,{});
   }
+  //user
+
   insert(userId : number): Observable<any>{
     return this.http.post(this.url + "/quiz/"+ userId ,{});
   }
   listQuizzesByUser(userId: number):Observable<any>{
     return this.http.get(this.url + "/quizzes/"+ userId);
+  }
+
+
+
+  getQuizById(quizId: number): Observable<any>{
+    return this.http.get(this.url + "/quiz/id/"+ quizId, {});
   }
 
 
