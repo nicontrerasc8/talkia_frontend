62d726657581405d9a665b57cc9063148c21a0e2
CamiTorres22 <u202217059@upc.edu.pe>
2024-11-15 01:18:40 -0500

    feat(frontend): Add edit user profile

--- src/app/business/admin/shared/nav-admin/nav-admin.component.ts
+++ src/app/business/admin/shared/nav-admin/nav-admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-nav-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './nav-admin.component.html',
   styleUrl: './nav-admin.component.css'
 })
--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,7 +1,8 @@
 import {inject, Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {User} from '../model/user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,54 @@ import {HttpClient} from '@angular/common/http';
 export class UsersService {
   private url = environment.apiUrl
   private http: HttpClient = inject(HttpClient);
+  private listaCambio: Subject<User[]> = new Subject<User[]>();
+
   constructor() {}
-  list():Observable<any>{
+  list(): Observable<any> {
     return this.http.get(this.url + "/users");
+  }
+
+  deleteUser(userId:number):  Observable<any>{
+    return this.http.delete(this.url + "/user/"+ userId);
+  }
+  getId(username: String): Observable<any> {
+    return this.http.get(this.url + "/user_get_id/" + username);
+  }
+
+  listId(id: number): Observable<any> {
+    return this.http.get(this.url + "/user/" + id);
+  }
+
+  insert(user: User): Observable<any> {
+    return this.http.post(this.url + "/user", user);
+  }
+
+  update(user: User): Observable<any> {
+    return this.http.put(this.url + "/user", user);
+  }
+
+  setList(listaNueva: User[]) {
+    this.listaCambio.next(listaNueva);
+  }
+
+  getList(): Observable<User[]> {
+    return this.listaCambio.asObservable();
+  }
+
+  listByUsername(username: String): Observable<any>{
+    return this.http.get(this.url + "/user_by_username/" + username);
+  }
+
+  getUserById(userId: number): Observable<any> {
+    return this.http.get(this.url + "/user/" + userId);
+  }
+
+
+  getUserAge(userId: number): Observable<any>{
+    return this.http.get(this.url +"/user/age/"+userId)
+  }
+
+  getStatusByUser(userId: number): Observable<any>{
+    return this.http.get(this.url + "/user/status/"+ userId, {responseType: "text"});
   }
 }
