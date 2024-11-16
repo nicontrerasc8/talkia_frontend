6b5fb654dbb8b31effebc9e2bb9a3a0e795072ce
Jamesrgm12 <rodrigoganto247@gmail.com>
2024-11-15 23:38:47 -0500

    feat(frontend): Add user login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import {Routes } from '@angular/router';
 import {LandingComponent} from './business/landing/landing.component';
-import {HomeComponent} from './home/home.component';
-import {ContentAdminComponent} from './home/pages/content-admin/content-admin.component';
-
-import UsersComponent from './home/pages/users/users.component';
-import { PaymentAdminComponent } from './home/pages/payment-admin/payment-admin.component';
-import { LoginComponent } from './login/login.component';
+import {LoginComponent} from './business/welcome/login/login.component';
 
 export const routes: Routes = [
   {
@@ -17,7 +12,7 @@ export const routes: Routes = [
   {
     path: 'home',
     children: [
-      { path: '', component: HomeComponent },
+      { path: '', component:  },
       { path: 'users', component: UsersComponent },
       { path: 'contents', component: ContentAdminComponent },
       { path: 'histories', component: PaymentAdminComponent },
---
+++ src/app/business/welcome/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+    imports: [
+        RouterLink,
+        RouterOutlet
+    ],
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted = false;
+
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+
+  ngOnInit(): void {
+    if(localStorage.getItem('token')!=null){
+      localStorage.removeItem('token');
+      console.log('T')
+    }
+    this.loginForm = this.formBuilder.group({
+      usuario: ['', Validators.required],
+      contrasena: ['', Validators.required]
+    });
+
+  }
+
+
+  // Método de envío del formulario
+  onSubmit() {
+    this.submitted = true;
+
+
+    // Si el formulario es inválido, no procede
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+
+    // Aquí puedes implementar la lógica de autenticación
+    if (this.loginForm.value.usuario === 'user' && this.loginForm.value.contrasena === 'user123') {
+      // Redirigir a otra ruta, por ejemplo, dashboard
+      this.router.navigate(['/user']);
+    } else {
+      alert('Usuario o contraseña incorrecta');
+    }
+  }
+}
